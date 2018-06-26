26df6249edbe77f44b10caac6de222a5c3a5c135
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-06-25 21:48:39 -0300

    modify comments to JSdoc

--- js-dev/app.js
+++ js-dev/app.js
@@ -1,19 +1,36 @@
-// Tells the player the game's objective
+/**
+ * @file this file holds the Enemy and Player classes/constructors, respective methods and a Constructor function for the variables used,
+ * @author Ricardo Bossan ricardobossan@gmail.com
+ *
+ *@todo
+ 	* pass `allEnemies` and `enemiesY arrays into the `gV` constructor
+ 	* include them into the constructor's parameter list and the respective arguments in it's instances
+ 	* adjust all code to this change
+ */
+
+// Tells the player what the game's objective is
 (() => {
 	window.alert(`Help the 5 children make it to the other side!`);
 })();
 
-// The gV object (instance of the following constructor function), will provide
-// it's properties as variables for use in most functions withing this game's code,
-// as oposed to using global variables, which could be changed by users in the console)
-// (as a clarification: The 'gV' object name would stand as short for "global variables")
+/**
+ * The gV object (instance of the following constructor function), will provide it's properties as variables for use in most functions withing this game's code, as oposed to using global variables, which could be changed by users in the console) (as a clarification: The 'gV' object name would stand as short for "global variables")
+ * @constructor
+ * @param {number} s seconds, for time() andEnemy.prototpe.update() functions
+ * @param {number} m minutes, for time() andEnemy.prototpe.update() functions
+ * @param {number} h hours, for time() andEnemy.prototpe.update() functions
+ * @param {number} round increments each time the player arrives at water
+ * @param {number} gameSpeed increments each round
+ * @param {number} arrivedXTimes defines the player's sprite for that round
+ * @param {number} score increments each round
+ * @param {function} scoreCounter returns incremented score
+ * @param {string} sprite object's sprite
+ * @param {array} highScore player's name/score, updated upon score, to be saved and loaded, on localStorage
+ */
 function Variables (
-	// seconds, minutes and hours for logic of the timer() function and the
-	// Enemy.prototype.update() method
 	s,
 	m,
 	h,
-	// ramaining parameters for use in functions
 	round,
 	gameSpeed,
 	arrivedXTimes,
@@ -34,12 +51,12 @@ function Variables (
 	this.highScore = highScore;
 }
 
-// Instantiates the Variables() Constructor Function
-// The arguments passed here, in the Variables() Constructor, work as values for
-// the properties to be used in this code's functions. This kind of creates
-// "secure global variables", for us in functions
-// When referenced by a function, each property of the gV object will be prefixed by
-//  this object's name
+/*
+ * Instantiates the Variables() Constructor Function
+ * @instance
+ * The arguments passed here work as values for the properties to be used in this code's functions. This kind of creates "secure global variables", for use in functions
+ * When referenced by a function, each property of the gV object will be prefixed by this object's name (gV)
+ */
 const gV = new Variables(
 	0,
 	0,
@@ -59,7 +76,10 @@ const gV = new Variables(
 	""
 );
 
-// Logic for timer
+/*
+ * Logic for timer
+ * @function
+ */
 const timer = function myTimer() {
 	gV.s++;
 	if (gV.s >= 60) {
@@ -71,13 +91,19 @@ const timer = function myTimer() {
 		}
 	}
 
-	// Sets the timer to run on 1 sec intervals, each time the function iterates over itself, until the game is through
+	// sets the timer to run on 1 sec intervals, each time the function iterates over itself, until the game is through
 	setTimeout(myTimer, 1000);
 };
-// runs timer function after 1 second
+// Runs timer function after 1 second
 setTimeout(timer, 1000);
 
-// Enemy's constructor function
+/*
+ * Enemy Class
+ * @constructor
+ * @param {number} x enemy's x axis position
+ * @param {number} y enemy's y axis position
+ * @param {number} speed random number generated for use in the Enemy.prototype.update() method
+ */
 let Enemy = function(x, y, speed) {
 
 	this.sprite = 'images/enemy-bug.png';
@@ -87,28 +113,31 @@ let Enemy = function(x, y, speed) {
 	this.width = 89;
 	this.height = 71;
 };
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/*
+ * Updates the enemy's position, required method for game
+ * @memberof Enemy()
+ * @prop
+ * @param {number} dt a time delta between ticks
+ */
 Enemy.prototype.update = function(dt) {
-
-	// Multiplies any movement by the dt parameter
-	// which will ensure the game runs at the same speed for
-	// all computers.
+	// Multiplies any movement by the dt parameter, which will ensure the game runs at the same speed for all computers
 	this.x += this.speed * dt;
 	// Checks for colliision between the player and enemy objects
 	(function() {
 		(function() {
-			// Checks collision for each of the 3 enemy bugs
-			// if the player arrives at a enemy position, including
-			//  it's width, collision occurs
+			/*
+			 * Checks collision for each of the 3 enemy bugs
+			 * If the player arrives at a enemy position, including it's width, collision occurs
+			 */
 			for(let i = 0; i < allEnemies.length; i++) {
 				if ((player.x < allEnemies[i].x + allEnemies[i].width) &&
 				(player.x + player.width > allEnemies[i].x) &&
 				(player.y < allEnemies[i].y + allEnemies[i].height) &&
 				(player.height + player.y > allEnemies[i].y)) {
-					// collision detected!
-					// reset the player position to it's starting point
+					/*
+					 * collision detected!
+					 * reset the player position to it's starting point
+					 */
 					setTimeout(function() {
 						player.resetPosition();
 						(function askName() {
@@ -125,17 +154,15 @@ Enemy.prototype.update = function(dt) {
 									["Pippin", 10]
 								];
 							}
-							// When the player is hit by a bug, ends the game and prompts the player
-							// for his/her name
+							// When the player is hit by a bug, ends the game and prompts the player for his/her name
 							var person = window.prompt("Game Over! Please, enter your name:", "Name");
 							if(person === null || person === "Name" || person === "") {
 								window.alert("Don't be shy!");
 								askName();
-							// Checks if the player's score reache's one of the 5 score marks, and if
-							// his score is higher than that of players from previous game sessions for
-							// that score mark. If all are `True`, then the present game session
-							// player's name and score will take the previous best score for that mark
-							// in the `HIGH SCORE`board
+							/*
+							 * Checks if the player's score reache's one of the 5 score marks, and if his score is higher than that of players from previous game sessions for that score mark.
+							 * If all are `True`, then the present game session player's name and score will take the previous best score for that mark in the `HIGH SCORE`board.
+							 */
 							} else {
 								if(gV.scoreCounter >= 50 && gV.scoreCounter >= gV.highScore[0][1]) {
 									gV.highScore[0][0] = person;
@@ -157,8 +184,7 @@ Enemy.prototype.update = function(dt) {
 									gV.highScore[4][0] = person;
 									gV.highScore[4][1] = gV.scoreCounter;
 								}
-								// saves current `HIGH SCORES` board names and scores for future game sessions
-								// even if the browser window is shut
+								// saves current `HIGH SCORES` board names and scores for future game sessions even if the browser window is shut
 								localStorage.setItem( 'stringfiedHScore', JSON.stringify(gV.highScore));
 								// displayes updated `HIGH SCORES` board
 								window.alert(`HIGH SCORE: \n\n${JSON.parse(localStorage.getItem("stringfiedHScore"))[0][0]} :  ${JSON.parse(localStorage.getItem("stringfiedHScore"))[0][1]}\n${JSON.parse(localStorage.getItem("stringfiedHScore"))[1][0]} :  ${JSON.parse(localStorage.getItem("stringfiedHScore"))[1][1]}\n${JSON.parse(localStorage.getItem("stringfiedHScore"))[2][0]} :  ${JSON.parse(localStorage.getItem("stringfiedHScore"))[2][1]}\n${JSON.parse(localStorage.getItem("stringfiedHScore"))[3][0]} :  ${JSON.parse(localStorage.getItem("stringfiedHScore"))[3][1]}\n${JSON.parse(localStorage.getItem("stringfiedHScore"))[4][0]} :  ${JSON.parse(localStorage.getItem("stringfiedHScore"))[4][1]}`);
@@ -181,13 +207,23 @@ Enemy.prototype.update = function(dt) {
 	}
 };
 
-// Draw the enemy on the screen
+/*
+ * Draw the enemy on the screen.
+ * @memberof Enemy
+ * @prop
+ */
 Enemy.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
 };
 
-// Player's constructor function
+/*
+ * Player Class
+ * @constructor
+ * @param {number} x player's position at x axis
+ * @param {number} y player's position at y axis
+ * @param {string} sprite player's sprite
+ */
 var Player = function(x, y, sprite) {
 	this.sprite = sprite;
 	this.x = x;
@@ -215,14 +251,17 @@ var Player = function(x, y, sprite) {
 		gV.arrivedXTimes = 0;
 	};
 };
-
-// Draw the enemy on the screen
+/*
+ * Draw the enemy on the screen
+ * @memberof Player
+ * @prop
+ */
 Player.prototype.render = function() {
 
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
-	// Each time the player reaches the water, updates the
-	// round counter, at top-left corner of the canvas
+	/*
+	 * Each time the player reaches the water, updates the	round counter, at top-left corner of the canvas
+	 */
 	ctx.font = '45px impact';
 	ctx.fillStyle = 'white';
 	ctx.fillText(`Round ${gV.round}`, 10, 108);
@@ -234,20 +273,21 @@ Player.prototype.render = function() {
 	ctx.fillText(`${gV.h > 9 ? gV.h : "0" + gV.h} : ${gV.m > 9 ? gV.m : "0" + gV.m} : ${gV.s > 9 ? gV.s : "0" + gV.s}`, 380, 82);
 	ctx.strokeText(`${gV.h > 9 ? gV.h : "0" + gV.h} : ${gV.m > 9 ? gV.m : "0" + gV.m} : ${gV.s > 9 ? gV.s : "0" + gV.s}`, 380, 82);
 
-	// updates the score counter, at the top-right corner of the canvas,
-	// each time the player reaches the water
+	// updates the score counter, at the top-right corner of the canvas, each time the player reaches the water
 	ctx.font = '30px impact';
 	ctx.fillStyle = 'white';
 	ctx.fillText(`Score: ${gV.score}`, 380, 118);
 	ctx.strokeText(`Score: ${gV.score}`, 380, 118);
 };
-
-// Creates 3 instances of the Enemy constructor, at the positions provided by
-// the enemiesY array, and adds them to the allEnemies array,
-// to be provided to the updateEntities() and renderEntities() functions
-// at the `engine.js` file
+/*
+ * Creates 3 instances of the Enemy constructor, at the positions provided by
+ the enemiesY array, and adds them to the allEnemies array,
+ to be provided to the updateEntities() and renderEntities() functions
+ at the `engine.js` file
+ */
 let allEnemies = [];
 let enemiesY = [63, 143, 223];
+
 for(let enemyY of enemiesY) {
 	let enemy = new Enemy(0, enemyY, 150 + (gV.gameSpeed * (Math.floor(Math.random() * 400))));
 	allEnemies.push(enemy);
@@ -292,8 +332,11 @@ Player.prototype.handleInput = function(key) {
 	}
 };
 
-// This listens for key presses and sends the keys to
-// Player.handleInput() method
+/*
+ * @event
+ * @listens for key presses
+ * @desc sends the keys to Player.prototype.handleInput() method
+ */
 document.addEventListener('keyup', function (e) {
 	var allowedKeys = {
 		37: 'left',
@@ -305,14 +348,22 @@ document.addEventListener('keyup', function (e) {
 	player.handleInput(allowedKeys[e.keyCode]);
 });
 
-// prevents window from rolling up or down
+/*
+ * @event
+ * @listens for key presses
+ * @desc prevents window from rolling up or down
+ */
 window.addEventListener("keydown", function(e) {
 	if ([38, 40].indexOf(e.keyCode) > -1) {
 		e.preventDefault();
 	}
 }, false);
 
-// prevents window from rolling left or right
+/*
+ * @event
+ * @listens for key presses
+ * @desc prevents window from rolling left or right
+ */
 window.addEventListener("keydown", function(e) {
 	if ([37, 39].indexOf(e.keyCode) > -1) {
 		e.preventDefault();
